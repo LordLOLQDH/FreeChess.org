a35ccecabb76cf5ae3475a94a82f3c67e9d7db41
LordLOLQDH <adam_kraus@icloud.com>
2026-08-26 18:26:08 +0200

    Fix: Add touch event support for mobile devices - Schach funktioniert jetzt auf Handy

--- controls.js
+++ controls.js
@@ -20,23 +20,28 @@ sprite.onload = () => {
     update();
     drawBoard();
 
-    document.addEventListener('mousedown', (e) => {
+    // Unified handler for both mouse and touch
+    function handleDown(e) {
         isDown = true;
         isUp = false;
 
-        let mouseX = e.clientX - cvs.getBoundingClientRect().left;
-        let mouseY = e.clientY - cvs.getBoundingClientRect().top;
+        const rect = cvs.getBoundingClientRect();
+        let mouseX, mouseY;
+
+        if (e.touches) {
+            // Touch event
+            mouseX = e.touches[0].clientX - rect.left;
+            mouseY = e.touches[0].clientY - rect.top;
+        } else {
+            // Mouse event
+            mouseX = e.clientX - rect.left;
+            mouseY = e.clientY - rect.top;
+        }
 
         let boardIndex = getBoardIndex(mouseX, mouseY);
-        // if we click on a square and its not empty,
-        // we want to remove the piece from the board
-        // and store it in the draggedPiece variable
 
         board.boardArr.forEach((sqr, i) => {
             if (sqr !== 0 && i == boardIndex) {
-
-                // but we first need to check whose turn is it first
-                // we use the whiteTurn variable and check if it corresponds with the first letter of the piece 
 
                 if ((whiteTurn && sqr.startsWith('w')) || (whiteTurn == false && sqr.startsWith('b'))) {
                     board.boardArr[i] = 0;
@@ -53,31 +58,48 @@ sprite.onload = () => {
         })
         update()
         drawBoard()
-    })
+    }
 
-    document.addEventListener('mousemove', (e) => {
+    function handleMove(e) {
         ctx.clearRect(0, 0, cvs.width, cvs.height);
         update();
 
-        let mouseX = e.clientX - cvs.getBoundingClientRect().left;
-        let mouseY = e.clientY - cvs.getBoundingClientRect().top;
+        const rect = cvs.getBoundingClientRect();
+        let mouseX, mouseY;
 
-        // if were dragging a piece, i.e draggedPiece !== null 
-        // we want to draw a 'fake' piece of the same type to follow the mouse
+        if (e.touches) {
+            // Touch event
+            mouseX = e.touches[0].clientX - rect.left;
+            mouseY = e.touches[0].clientY - rect.top;
+        } else {
+            // Mouse event
+            mouseX = e.clientX - rect.left;
+            mouseY = e.clientY - rect.top;
+        }
 
         if (isDown) {
             if (draggedPiece !== null) {
                 pieces.drawPiece(pieces.type[draggedPiece], [{ x: mouseX - pieces.pieceScale / 2, y: mouseY - pieces.pieceScale / 2 }]);
             }
         }
-    })
+    }
 
-    document.addEventListener('mouseup', (e) => {
+    function handleUp(e) {
         isDown = false;
         isUp = true;
 
-        let mouseX = e.clientX - cvs.getBoundingClientRect().left;
-        let mouseY = e.clientY - cvs.getBoundingClientRect().top;
+        const rect = cvs.getBoundingClientRect();
+        let mouseX, mouseY;
+
+        if (e.changedTouches) {
+            // Touch event
+            mouseX = e.changedTouches[0].clientX - rect.left;
+            mouseY = e.changedTouches[0].clientY - rect.top;
+        } else {
+            // Mouse event
+            mouseX = e.clientX - rect.left;
+            mouseY = e.clientY - rect.top;
+        }
 
         let boardIndex = getBoardIndex(mouseX, mouseY);
 
@@ -85,7 +107,7 @@ sprite.onload = () => {
         function reverseMovement() {
             board.boardArr[prevSqrIndex] = draggedPiece;
             board.boardArr[boardIndex] = capturedPiece;
-            whiteTurn = draggedPiece[0] == 'w' ? true : false; // change the turn to the person that made the reversed move
+            whiteTurn = draggedPiece[0] == 'w' ? true : false;
             halfMoveCount--;
             fullMoveCount = roundToWhole(halfMoveCount / 2);
             playStockFishMove = false;
@@ -97,10 +119,9 @@ sprite.onload = () => {
         }
         // --------------------------- //
 
-        whiteDangerSqrs = []; // reset danger squares
-        blackDangerSqrs = []; // reset danger squares
+        whiteDangerSqrs = [];
+        blackDangerSqrs = [];
 
-        // check for castling movement and if it is, allow castling
         if (canWhiteCastleRightSide(prevSqrIndex, draggedPiece, boardIndex)) {
             isCastle = true;
             whiteRightSideCastle();
@@ -133,19 +154,16 @@ sprite.onload = () => {
             playStockFishMove = false;
             audio.playAudio(audio.sound.move);
         }
-        // -------------------------------------------------------- //
 
-        // once we release our piece we want to get its new location and chage it
-        // update the boardArr data and update the game
         else if (draggedPiece !== null && possibleSqres.length > 0) {
             let isCapture = board.boardArr[boardIndex] == 0 ? false : true;
             for (let i = 0; i < possibleSqres.length; i++) {
                 if (boardIndex == possibleSqres[i]) {
-                    capturedPiece = board.boardArr[boardIndex] !== 0 ? board.boardArr[boardIndex] : 0; // if we captured a piece
+                    capturedPiece = board.boardArr[boardIndex] !== 0 ? board.boardArr[boardIndex] : 0;
                     board.boardArr[boardIndex] = draggedPiece;
                     board.boardArr[prevSqrIndex] = 0;
              
-                    whiteTurn = !whiteTurn; // switch turns
+                    whiteTurn = !whiteTurn;
                     halfMoveCount++;
                     fullMoveCount = roundToWhole(halfMoveCount / 2);
 
@@ -153,38 +171,34 @@ sprite.onload = () => {
                     if (whiteTurn == false) playStockFishMove = true;
                     else playStockFishMove = false;
 
-                    if (board.boardArr[boardIndex][1] == 'P') { // if it's a pawn
-                        pawnsThatHaveMovedPastOnce.push(boardIndex); // add it to the list of pawns moved more than once
+                    if (board.boardArr[boardIndex][1] == 'P') {
+                        pawnsThatHaveMovedPastOnce.push(boardIndex);
                     }
-                    // check for castling legality with every king or rook movement!
                     checkWhiteRightCastleLegality(prevSqrIndex, draggedPiece);
                     checkWhiteLeftCastleLegality(prevSqrIndex, draggedPiece);
                     checkBlackRightCastleLegality(prevSqrIndex, draggedPiece);
                     checkBlackLeftCastleLegality(prevSqrIndex, draggedPiece);
 
-                    // check if the king is on check
                     findWhiteDangerSqrs();
                     findBlackDangerSqrs();
 
                     if (whiteTurn == false) {
-                         // find out if the move the human made is a check
-                        if(getPossibleMoves(draggedPiece, boardIndex).includes(board.boardArr.indexOf('bK'))) {
+                         if(getPossibleMoves(draggedPiece, boardIndex).includes(board.boardArr.indexOf('bK'))) {
                             if(isCheck == false) isCheck = true;
                         } else {
                             isCheck = false;
                         }
                         //
-                        if (whiteDangerSqrs.includes(board.boardArr.indexOf('wK'))) { // if the current square of the wK is among the danger squares
-                            reverseMovement(); // reverse the movement until they have resolved the check
+                        if (whiteDangerSqrs.includes(board.boardArr.indexOf('wK'))) {
+                            reverseMovement();
                         }
                     } else {
                         playStockFishMove = false;
-                        if (blackDangerSqrs.includes(board.boardArr.indexOf('bK'))) { // if the current square of the bK is among the danger squares
-                            reverseMovement(); // reverse the movement until they have resolved the check
+                        if (blackDangerSqrs.includes(board.boardArr.indexOf('bK'))) {
+                            reverseMovement();
                         }
                     }
 
-                    // play the right audios
                     if(isCheck == false) {
                         if (isCapture) audio.playAudio(audio.sound.capture);
                         else audio.playAudio(audio.sound.move);
@@ -193,18 +207,25 @@ sprite.onload = () => {
                     }
                 }
             }
-            // if the square were trying to move to isnt part of the possibleMoves
-            // we move back to the prev square
             if (!possibleSqres.includes(boardIndex)) {
                 board.boardArr[prevSqrIndex] = draggedPiece;
             }
             resetMovement();
-        } else if (draggedPiece !== null && possibleSqres.length <= 0) { // if there are no squares to move to
+        } else if (draggedPiece !== null && possibleSqres.length <= 0) {
             board.boardArr[prevSqrIndex] = draggedPiece;
             resetMovement();
         }
         update();
-    })
-}
+    }
 
+    // Mouse events
+    document.addEventListener('mousedown', handleDown);
+    document.addEventListener('mousemove', handleMove);
+    document.addEventListener('mouseup', handleUp);
+
+    // Touch events
+    document.addEventListener('touchstart', handleDown);
+    document.addEventListener('touchmove', handleMove);
+    document.addEventListener('touchend', handleUp);
+}
 
